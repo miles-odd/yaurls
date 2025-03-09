1b408865fafcf7d3f753d7da81c73d7ac55f86c2
cybedra <147957633+cybedra@users.noreply.github.com>
2025-03-09 13:39:59 -0700

    basic insert/get url

--- app/database.py
+++ app/database.py
@@ -3,7 +3,43 @@ import sqlite3
 
 DB_FILE = "yaurls.db"
 
-# Create yaurls.db if it doesn't already exist in local project directory
+# Initialize the database (+ create it, if it doesn't exist in local project directory)
 def init_db():
     con = sqlite3.connect(DB_FILE)
+    cursor = con.cursor()
+    
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS urls (id INTEGER PRIMARY KEY, slug TEXT, url TEXT)
+    ''')
+    
     con.close()
+
+# Insert a new slug/url mapping into the database
+def insert_url(slug: str, url: str) -> None:
+    con = sqlite3.connect(DB_FILE)
+    cursor = con.cursor()
+    
+    cursor.execute('''
+        INSERT INTO urls (slug, url)
+        VALUES (?, ?)
+    ''', (slug, url))
+    
+    con.commit()
+    con.close() 
+    
+# Get the URL mapped to by the given slug
+def get_url(slug: str) -> str:
+    con = sqlite3.connect(DB_FILE)
+    cursor = con.cursor()
+    
+    cursor.execute('''
+        SELECT url FROM urls WHERE slug = ?
+    ''', (slug,))
+    
+    url = cursor.fetchone()
+    con.close()
+    
+    if url is not None:
+        return url[0]
+    else:
+        return None
