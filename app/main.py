7dffa78c6a29633aa0d469bb9c71a1529e3228dc
cybedra <147957633+cybedra@users.noreply.github.com>
2025-03-09 20:31:03 -0700

    finally fixed redirection

--- app/main.py
+++ app/main.py
@@ -1,11 +1,21 @@
 # Core application logic
 from fastapi import FastAPI, HTTPException
-from fastapi.responses import RedirectResponse
+from fastapi.responses import RedirectResponse, FileResponse
 from fastapi.middleware.cors import CORSMiddleware
+
+from pydantic import BaseModel
 
 from .database import *
 from .utils import *
 
+class URLRequest(BaseModel):
+    original_url: str
+    
+class Entry(BaseModel):
+    slug: str
+    url: str
+    visits: int
+    
 # Start FastAPI and make sure the database exists
 app = FastAPI()
 init_db()
@@ -22,45 +32,39 @@ app.add_middleware(
 # Test stuff with root route
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return FileResponse("frontend/index.html")
 
 # Create new slug to map to the given URL
 @app.post("/shorten")
-def shorten_url(url: str):
+def shorten_url(request: URLRequest) -> dict:
     slug = generate_random_slug(6)
-    insert_url(slug, url)
+    # Strip quotes from URL wrapped in JSON
+    url = request.original_url[1:-1]
+    insert_url(slug, request.original_url)
     
+    # Expected return of the form {"shortened_url": "yaurls.it/abc123"}
     short_url = "yaurls.it/" + slug
     return {"shortened_url": short_url}
 
 # Return the URL mapped to by the given slug
 @app.get("/{slug}")
-def expand_url(short_url: str):
-    
-    # Obtain just the slug if short_url contains the whole thing
-    if short_url.startswith("yaurls.it/"):
-        slug = short_url.removeprefix("yaurls.it/")
-    else:
-        slug = short_url
-        
+def expand_url(slug: str) -> str:
     url = get_url(slug)
     
     if url is None:
-        return {"error": "URL not found"}
+        raise HTTPException(status_code=404, detail="URL not found")
     else:
-        return {"retrieved": url}
+        return url
     
 # Redirect to the URL mapped to by the given slug
-@app.get("/redirect")
-def redirect_url(short_url: str):
-        
-    # Obtain just the slug if short_url contains the whole thing
-    if short_url.startswith("yaurls.it/"):
-        slug = short_url.removeprefix("yaurls.it/")
-    else:
-        slug = short_url
-        
+@app.get("/redirect/{slug}")
+def redirect_url(slug: str) -> RedirectResponse:
     url = get_url(slug)
-    print(f"Retrieved URL: {url}")
+    
+    if not url.startswith("http"):
+        url = "https://" + url
+
+    if url is None:
+        raise HTTPException(status_code=404, detail="URL not found")
     
     return RedirectResponse(url=url, status_code=307)
